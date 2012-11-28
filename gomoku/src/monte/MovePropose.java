26c8c87ecd2cb75abede7d05e87377f1d0728948
liujingtongjanet <liujingtongjanet@gmail.com>
2012-11-28 03:52:06 +0000

    聪明版monte啦啦

--- gomoku/src/monte/MovePropose.java
+++ gomoku/src/monte/MovePropose.java
@@ -2,6 +2,9 @@ package monte;
 
 import gomoku.Minimax;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.Random;
 
 //import java.util.*;
@@ -12,8 +15,8 @@ public class MovePropose {
    // finally, play the move
      Board a = new Board(); 
 	 private int side = 0;
-     private int[] wintime = null;
-	 private int[] posMove = null;
+ //    private int[] wintime = null;
+//	 private int[] posMove = null;
 	 private int[] bestfiveMoves = null;
 	 private int bestmove = 0;
 
@@ -40,7 +43,7 @@ public int playmove(int[]game,int playside) throws Exception{
 		for(int j = 0;j < a.size;j++)
 			a.boardtwo[i][j] = a.boardone[i + j * 15];
 
-	for(int i=0;i < 5; i++)
+	for(int i = 0;i < 5; i++)
 	 {
 		SmartSimulate(bestfiveMoves[i]);
 //		simulate(bestfiveMoves[i],side); // get the new winRateList
@@ -48,6 +51,14 @@ public int playmove(int[]game,int playside) throws Exception{
 	 }
 	    movesearch(); // update the new bestmove and bestfiveMoves
 	    System.out.println("hi, here is 2 " + bestmove);
+	    if(bestmove == 0)
+	    {
+	    	System.out.println("bestfivemove");
+	    	for(int i = 0 ; i < 225; i++)
+	    	{
+	    		System.out.println(a.winRate[i]);
+	    	}
+	    }
 	    return bestmove; 
 	    
 	
@@ -63,8 +74,8 @@ public int firstmove(int[]game,int playside) throws Exception{
 	bestfiveMoves[4] = a.winRate.length / 2 + a.size;
 	side = playside;
 	
-	wintime = new int[a.boardone.length];
-	posMove = new int[a.boardone.length];
+//	wintime = new int[a.boardone.length];
+//	posMove = new int[a.boardone.length];
 	
 	System.out.println("hi, this is" + bestmove);
 	for(int i=0;i < 5; i++)
@@ -81,14 +92,30 @@ public int firstmove(int[]game,int playside) throws Exception{
 // do the movesearch, return the best
 public void movesearch(){
 //initial the helper structures	
-for(int i = 0; i < a.winRate.length; i++){	
-	posMove[i] = i;
-	wintime[i] = a.winRate[i];
+	LinkedList<Wintime> wintimelist = new LinkedList<Wintime>();
+	for(int i = 0; i < a.winRate.length; i++){
+		if(a.boardone[i] == 0){
+			Wintime w = new Wintime();
+			w.index = i;
+			w.winrate = a.winRate[i];
+			wintimelist.add(w);
+		}else continue;
+		}
+	Collections.sort(wintimelist, new WintimeComparator());
+	bestmove = wintimelist.get(wintimelist.size() - 1).index;
+	for(int i = 0 ;i < 5;i++)
+	{
+		bestfiveMoves[i] = wintimelist.get(wintimelist.size() - 2 - i).index;
 	}
+	
+	
+
+
+/*
 	int[] tempwin = new int[6];
 	int[] tempposmove = new int[6];
 	for(int i = 0; i < 6;i++){
-		tempwin[i] = 0;
+		tempwin[i] = -1000000000;
 		tempposmove[i] = 0;
 	}
 	//find the best six moves
@@ -110,11 +137,13 @@ for(int i = 0; i < a.winRate.length; i++){
 			   }
 			}	
 	}
+	
 	//new bestmove and bestfivemoves for next step;
 	bestmove = tempposmove[5];
 	for(int i = 0; i < 5;i++){
 		bestfiveMoves[i] = tempposmove[i + 1];
 	}
+	*/
 	}
   
 	 
@@ -243,6 +272,7 @@ for(int i = 0; i < a.winRate.length; i++){
         if(a.isWin(side)){
             a.updateWinRate(a.winRateList,side,true); 
             a.updatetotalrate(a.winRateList);
+            return;
         }
         
         
@@ -250,7 +280,7 @@ for(int i = 0; i < a.winRate.length; i++){
         //oppose play in minmax
         Minimax mn = new Minimax(a.boardone);
     	int m = mn.getBestMove(0 - side, 2);
-//    	System.out.println("hi3.5!\n");
+//    	System.out.println("hi!"+ m +"\n");
     	a.boardone[m] = 0 - side;
     	int xCoord1 = m % 15;
     	int yCoord1 = m / 15;
@@ -266,29 +296,23 @@ for(int i = 0; i < a.winRate.length; i++){
     	//simulate
         for(int i = 0; i < 1000;i++)       	 
         {
-        //	System.out.println("hi1!\n");
-       // 	System.out.println("hi2!\n");
+
         	int y = playRandomLegalMove();
          	 simulateplay(y,side);
-    //        Simulateplay();
-    //        System.out.println("hi2!\n"); 
+
         	a.clearboard();
         	
         	a.boardone[x] = side; 
-        	a.boardtwo[xCoord][yCoord] = side;    	
-     
+        	a.boardtwo[xCoord][yCoord] = side;    	     
         	a.winRateList.add(playmove);   		    	
         	
         	//judge win;
             if(a.isWin(side)){
                 a.updateWinRate(a.winRateList,side,true); 
                 a.updatetotalrate(a.winRateList);
+                return;
             }
-            
-            
-     //     System.out.println("hi1!\n");
             //oppose play in minmax
-//        	System.out.println("hi3.5!\n");
         	a.boardone[m] = 0 - side;
         	a.boardtwo[xCoord1][yCoord1] = 0 - side;
         	if(a.isWin(0 - side))
@@ -302,54 +326,17 @@ for(int i = 0; i < a.winRate.length; i++){
        	
        	}
     }
-	public void SmartSimulateplay() throws Exception
-	{
-		if(a.isfull())return;
-    	
-		while(true){
-			//minmax is the expert opposer
-			////////////////////this part has problem!!!!!!!!!!!!
-	//		System.out.println("hi3!\n");
-	    	Minimax mn = new Minimax(a.boardone);
-	    	int m = mn.getBestMove(side, 2);
-	//    	System.out.println("hi3.5!\n");
-	    	a.boardone[m] = 0 - side;
-	    	int xCoord = m % 15;
-	    	int yCoord = m / 15;
-	    	a.boardtwo[xCoord][yCoord] = 0 - side;
-	    	if(a.isWin(0 - side))
-	    		{
-	    	//	System.out.println("hiwin1!\n");
-	    		 a.updateWinRate(a.winRateList,side,false); // 
-	         	 a.reducetotalrate(a.winRateList);
-	    		return;
-	    		}
-	    	if(a.isfull())return;
-	//    	System.out.println("hi4!"+ m + "\n");
-	    	////////////////////problem over!!!!!!!!!!!!
-	    	
-	    	
-	    	//monte use random  
-	    	int x = playRandomLegalMove();
-	    	a.boardone[x] = side;
-	    	int xCoord1 = x % 15;
-	    	int yCoord1 = x / 15;
-	    	a.boardtwo[xCoord1][yCoord1] = side;
-	    	Move playmove1 = new Move(x);
-        	a.winRateList.add(playmove1); 
-	    	if(a.isWin(side)){
-	    	//	System.out.println("hiwin2!\n");
-	    		a.updateWinRate(a.winRateList,side,true);
-     			a.updatetotalrate(a.winRateList);
-	    		return;
-	    	}
-	    	if(a.isfull())return;
-	//    	System.out.println("hi5!\n");
-		}	
-    
+	private class WintimeComparator implements  Comparator<Wintime>{
+		@Override
+		public int compare(Wintime o1, Wintime o2) {
+			return  o1.winrate.compareTo(o2.winrate);
+		}
 	}
-        
+	public class Wintime {
 		
-	
+		Integer winrate;
+		int index;
+		
 
+	}
 }
